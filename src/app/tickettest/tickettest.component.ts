8f059d46aa0fff12c00131304ea40b5a9b91e3ab
HaoGeng <aooaao1234@gmail.com>
2017-07-26 00:24:58 -0500

    stripe wip

--- src/app/tickettest/tickettest.component.ts
+++ src/app/tickettest/tickettest.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from "../http.service";
 
+declare var Stripe: any;
+
 @Component({
   selector: 'app-tickettest',
   templateUrl: './tickettest.component.html',
@@ -13,7 +15,8 @@ export class TickettestComponent implements OnInit {
   ticketQuantities: number[];
   seats: any[];
   selectedSeatList: number[];
-  selectedSeats: number[] = [];
+  //stripe: any;
+  //elements: any;
 
   eventName = "event title";
 
@@ -23,6 +26,59 @@ export class TickettestComponent implements OnInit {
 
   ngOnInit() {
     console.log(this.eventId);
+    var stripe = Stripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
+    var elements = stripe.elements();
+
+    var card = elements.create('card', {
+      hidePostalCode: true,
+      style: {
+        base: {
+          iconColor: '#666EE8',
+          color: '#31325F',
+          lineHeight: '40px',
+          fontWeight: 300,
+          fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+          fontSize: '15px',
+
+          '::placeholder': {
+            color: '#CFD7E0',
+          },
+        },
+      }
+    });
+    card.mount('#card-element');
+
+    function setOutcome(result) {
+      var successElement = document.querySelector('.success');
+      var errorElement = document.querySelector('.error');
+      successElement.classList.remove('visible');
+      errorElement.classList.remove('visible');
+
+      if (result.token) {
+        // Use the token to create a charge or a customer
+        // https://stripe.com/docs/charges
+        successElement.querySelector('.token').textContent = result.token.id;
+        successElement.classList.add('visible');
+      } else if (result.error) {
+        errorElement.textContent = result.error.message;
+        errorElement.classList.add('visible');
+      }
+    }
+
+    card.on('change', function(event) {
+      setOutcome(event);
+    });
+
+    document.querySelector('form').addEventListener('submit', function(e) {
+      e.preventDefault();
+      var form = document.querySelector('form');
+      var extraDetails = {
+        name: (<HTMLInputElement>form.querySelector('input[name=cardholder-name]')).value,
+        username: (<HTMLInputElement>form.querySelector('input[name=user-name]')).value,
+        useremail: (<HTMLInputElement>form.querySelector('input[name=user-email]')).value,
+      };
+      stripe.createToken(card, extraDetails).then(setOutcome);
+    });
 
     this.http.getTicketInfo(this.eventId).subscribe(
         data => {
@@ -43,6 +99,23 @@ export class TickettestComponent implements OnInit {
             alert(error);
         }
     );
+  }
+
+  setOutcome (result) {
+    var successElement = document.querySelector('.success');
+    var errorElement = document.querySelector('.error');
+    successElement.classList.remove('visible');
+    errorElement.classList.remove('visible');
+
+    if (result.token) {
+      // Use the token to create a charge or a customer
+      // https://stripe.com/docs/charges
+      successElement.querySelector('.token').textContent = result.token.id;
+      successElement.classList.add('visible');
+    } else if (result.error) {
+      errorElement.textContent = result.error.message;
+      errorElement.classList.add('visible');
+    }
   }
 
   // checkInArray(key) {
