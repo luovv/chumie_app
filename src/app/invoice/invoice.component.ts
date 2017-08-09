eb8643e9177ff16a6578c65a869e883eef40265c
Michael_Jianan_Han <jianan.h@gmail.com>
2017-08-09 16:31:58 -0400

    update invoice component

--- src/app/invoice/invoice.component.ts
+++ src/app/invoice/invoice.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router} from '@angular/router';
+import { ActivatedRoute, Router, Params} from '@angular/router';
 import { HttpService } from '../http.service';
 
 @Component({
@@ -10,7 +10,7 @@ import { HttpService } from '../http.service';
 })
 export class InvoiceComponent implements OnInit {
 
-  private invoiceId ='596c2ed653db18ff704783b7';
+  private invoiceId = '';//'596c2ed653db18ff704783b7';
   private payer: string = '';
   private nowDate = new Date();
   private date: Date;
@@ -25,9 +25,13 @@ export class InvoiceComponent implements OnInit {
   private message2 = 'For next step, you can join the group chat for this event.' +
   ' We will keep updating information in the chat, please make sure you stay connected.';
 
-  constructor(private router: Router, private http: HttpService, route: ActivatedRoute) { }
+  constructor(private router: Router, private http: HttpService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.forEach((params: Params) => {
+      console.log(params);
+      this.invoiceId = params['invoiceId'];
+    });
     this.http.getInvoice(this.invoiceId).subscribe(
       data => {
         this.payer = data.invoice.payer;
@@ -35,7 +39,8 @@ export class InvoiceComponent implements OnInit {
         this.title = data.invoice.title;
         this.amount_due = data.invoice.amount_due;
         this.num = data.num;
-        this.groupId = data.invoice._id;
+        this.groupId = data.invoice.chargeItemId;
+        console.log('GroupId:' + this.groupId);
         console.log(this.num);
         console.log(this.payer);
       },
