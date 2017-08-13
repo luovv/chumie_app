6c3ff60644e0bb82b32042210070a995a8a52470
Michael Han <jianan.h@gmail.com>
2017-08-13 13:37:47 -0400

    Fix mobile input issue for chat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,7 +41,8 @@ import {TranslateModule, TranslateLoader} from "@ngx-translate/core";
 import {TranslateHttpLoader} from "@ngx-translate/http-loader";
 import { ChatMobileComponent } from './chat/chat-mobile/chat-mobile.component';
 import { GroupchatComponent } from './groupchat/groupchat.component';
-import { MobileNavComponent } from './mobile-nav/mobile-nav.component';//import { ShareComponent } from './circle/share/share.component';
+// import { MobileNavComponent } from './mobile-nav/mobile-nav.component';
+//import { ShareComponent } from './circle/share/share.component';
 export function HttpLoaderFactory(http: Http) {
   return new TranslateHttpLoader(http);
 }
@@ -77,8 +78,7 @@ export function HttpLoaderFactory(http: Http) {
     GroupComponent,
     InvoiceComponent,
     ChatMobileComponent,
-    GroupchatComponent,
-    MobileNavComponent
+    GroupchatComponent
     //ShareComponent
   ],
   imports: [
