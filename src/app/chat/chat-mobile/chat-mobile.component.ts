3059793db0c5bed4ecffc2f27e4bf8926a595cf9
Michael Han <jianan.h@gmail.com>
2017-08-14 01:21:04 -0400

    Fix bug for mobile input view cover

--- src/app/chat/chat-mobile/chat-mobile.component.ts
+++ src/app/chat/chat-mobile/chat-mobile.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit, Output, Input, EventEmitter, AfterViewChecked} from '@angular/core';
+import  "../../../assets/js/jquery.js";
 
+declare var $: any;
 @Component({
   selector: 'app-chat-mobile',
   templateUrl: './chat-mobile.component.html',
@@ -14,7 +16,18 @@ export class ChatMobileComponent implements OnInit, AfterViewChecked {
 
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    var bfscrolltop = document.body.scrollTop;
+
+    $("input-box.myMessage").focus(function(){
+      var interval = setInterval(function(){
+        document.body.scrollTop = document.body.scrollHeight;
+      },200)
+    }).blur(function(){//设定输入框失去焦点时的事件
+      // clearInterval(interval);//清除计时器
+      document.body.scrollTop = bfscrolltop;
+    });
+  }
   pressKey(key){
     if(key.key=='Enter'){
       this.sendMessage.emit(this.myMessage);
@@ -22,14 +35,14 @@ export class ChatMobileComponent implements OnInit, AfterViewChecked {
     }
   }
 
-  adjustPage(event){
-    var inputTextBox = document.getElementById('chat-window-input-box');
-    setInterval(function(){
-      inputTextBox.scrollIntoView(false);
-      // inputTextBox.scrollIntoView(true);
-      // inputTextBox.scrollIntoViewIfNeeded();
-    },200);
-  }
+  // adjustPage(event){
+  //   var inputTextBox = document.getElementById('chat-window-input-box');
+  //   setInterval(function(){
+  //     inputTextBox.scrollIntoView(false);
+  //     // inputTextBox.scrollIntoView(true);
+  //     // inputTextBox.scrollIntoViewIfNeeded();
+  //   },200);
+  // }
 
   onSentMsgBtnClick(event){
 
